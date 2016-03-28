dc8b28c142097682b2b57fc851cc258e83ae68a2
Paul Balaji <paulmonish@gmail.com>
2016-03-28 23:38:54 +0100

    deleted blah.py

--- blah.py
+++
@@ -1,3 +0,0 @@
-for i in range(1, 17):
-    # print "<img src='images/hero/" + str(i) + ".jpg' style='width: 0%; height: 0%'>"
-    print "<link rel='prefetch' href='images/hero/" + str(i) + ".jpg'>"
